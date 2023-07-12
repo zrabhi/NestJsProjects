b8b635867921a87ecb00c3707ca97aeada30e395
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-07-12 01:35:02 +0100

    minor changes CreateNinjaDto

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,9 +1,9 @@
 
 import {MinLength, MaxLength, IsEnum} from 'class-validator'
 export class CreateNinjaDto {
-    @MinLength(1,{message: 'Not enough Length'})
+    @MinLength(1,{message: 'NMin length is one word'})
     name : string;
 
-    @IsEnum(['starts, nunchucks'], {message: 'Chose another weapon '})
+    @IsEnum(['starts', 'nunchucks'], {message: 'Chose another weapon '})
     weapon : 'starts' | 'nunchucks';
 }
